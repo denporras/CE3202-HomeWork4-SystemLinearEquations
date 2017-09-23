d8738d37f0d600fffba6c811a5dc1fe02fcab8e4
denporras <denporras07@gmail.com>
2017-09-23 12:38:42 -0600

    [ADD]First commit with the matrix class

---
+++ SolveSystemsLinearEquation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Resolucion de Sistemas de Ecuacione Lineales
+// Author      : Kevin Alfaro Vega, Dennis Porras Barrantes, David Gómez Vargas
+// Version     : 1.0
+// Copyright   : Assignment for the course Numerical Analysis of the Costa Rica Institute of Technology
+// Description : Program that solves systems of linear equations.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Jaja Saldos" << endl;
+	return 0;
+}
