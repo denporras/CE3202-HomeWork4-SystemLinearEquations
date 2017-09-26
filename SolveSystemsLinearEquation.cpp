85580f3484c0fb303fe9ce60068f2202a87c1860
Daedgomez <daedgomez@gmail.com>
2017-09-25 22:40:38 -0600

    [Add] QR Descomposition

--- SolveSystemsLinearEquation.cpp
+++ SolveSystemsLinearEquation.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Resolucion de Sistemas de Ecuacione Lineales
-// Author      : Kevin Alfaro Vega, Dennis Porras Barrantes, David Gómez Vargas
+// Author      : Kelvin Alfaro Vega, Dennis Porras Barrantes, David Gómez Vargas
 // Version     : 1.0
 // Copyright   : Assignment for the course Numerical Analysis of the Costa Rica Institute of Technology
 // Description : Program that solves systems of linear equations.
@@ -15,6 +15,7 @@
 #include <cstdlib>
 
 #include "Descomposition/LUDescomposition.h"
+#include "Descomposition/QRDescomposition.h"
 
 using namespace std;
 using namespace anpi;
@@ -31,10 +32,19 @@ void printMatrix(Matrix<T> &m){
 
 
 int main() {
-	Matrix<double> A= {{25,5,1},{64,8,1},{144,12,1}};
+	Matrix<double> A= {{2, 1, 3, 5},{-1, 0, 7, 1},{0, -1, -1, 3},{-3, 7, 4, 3},{1, 6, 4, -3}};
+	Matrix<double> Q;
+	Matrix<double> R;
+	QRDescomposition<double> * des = new QRDescomposition<double>();
+	des->qr(A,Q,R);
+	printMatrix(Q);
+	printMatrix(R);
+	
 	Matrix<double> LU;
 	LUDescomposition<double> * d = new LUDescomposition<double>();
 	d->lu(A, LU);
 	printMatrix(LU);
+	
+
 	return 0;
 }
