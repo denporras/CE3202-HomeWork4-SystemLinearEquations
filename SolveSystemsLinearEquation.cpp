6ac1047c76bb2e897227dccf0c55575b5ad3edb9
denporras <denporras07@gmail.com>
2017-09-27 16:10:30 -0600

    [ADD] inverse and new class Matrix descomposition

--- SolveSystemsLinearEquation.cpp
+++ SolveSystemsLinearEquation.cpp
@@ -16,6 +16,7 @@
 
 #include "Descomposition/LUDescomposition.h"
 #include "Descomposition/QRDescomposition.h"
+#include "Descomposition/MatrixDescomposition.h"
 
 using namespace std;
 using namespace anpi;
@@ -40,19 +41,36 @@ void printVector(vector<T> &v){
 }
 
 int main() {
-	Matrix<double> A= {{1,1,3},{1,2,4},{4,5,7}};
-	LUDescomposition<double> * d = new LUDescomposition<double>();
+	Matrix<double> A = {{1,0,5,7,0,7},
+						{3,3,44,6,8,8},
+						{6,4,3,3,4,6},
+						{89,8,7,6,5,4},
+						{5,6,8,8,9,8},
+						{65,4,3,3,4,5}};
 	vector<double> x;
-	vector<double> b = {{7,11,20}};
-	d->solve(A, x, b);
+	vector<double> b = {{4,7,7,4,7,7}};
+	MatrixDescomposition<double> * d = new MatrixDescomposition<double>();
+	d->solveLU(A, x, b);
 	printVector(x);
 
+	cout << endl;
+
+	Matrix<double> M = {{5,6,4},
+						{5,8,8},
+						{5,1,3}};
+	Matrix<double> Mi;
+	d->inverse(M, Mi);
+	printMatrix(Mi);
+
+	/*
 	Matrix<double> A_1= {{2, 1, 3, 5},{-1, 0, 7, 1},{0, -1, -1, 3},{-3, 7, 4, 3},{1, 6, 4, -3}};
 	Matrix<double> Q;
 	Matrix<double> R;
+	R = Matrix<double>(2,2,double(0));
+	printMatrix(R);
 	QRDescomposition<double> * des = new QRDescomposition<double>();
 	des->qr(A_1,Q,R);
-	printMatrix(Q);
-	printMatrix(R);
+	//printMatrix(Q);
+	//printMatrix(R);*/
 	return 0;
 }
