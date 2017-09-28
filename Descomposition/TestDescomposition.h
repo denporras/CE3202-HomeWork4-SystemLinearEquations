ed0e48eb8dc5dc326da8af2c82ebff71c5f10427
kevin <kevinalfa17@gmail.com>
2017-09-28 15:26:01 -0600

    unity test

--- Descomposition/TestDescomposition.h
+++ Descomposition/TestDescomposition.h
@@ -10,6 +10,7 @@
 
 #include "../Matrix/Matrix.hpp"
 #include <cmath>
+#include <gtest/gtest.h>
 
 template<typename T>
 class TestDescomposition {
@@ -24,12 +25,13 @@ private:
 };
 
 template<typename T>
-TestDescomposition<T>::TestDescomposition() {
-
-}
+TestDescomposition<T>::TestDescomposition() {}
 
 template<typename T>
 T TestDescomposition<T>::testLU(anpi::Matrix<T> &A, anpi::Matrix<T> &LU){
+
+	ASSERT_EQ(A.rows(),A.cols());   //A matrix is square
+	ASSERT_EQ(LU.rows(),LU.cols()); //LU matrix is square
 
 	anpi::Matrix<T> LT(LU.rows(),LU.cols(),T(0));
 	anpi::Matrix<T> UT(LU.rows(),LU.cols(),T(0));
@@ -38,31 +40,31 @@ T TestDescomposition<T>::testLU(anpi::Matrix<T> &A, anpi::Matrix<T> &LU){
 	this->getUpperLowerTringular(LU,UT,LT);
 
 	A_aux = LT*UT;
-	//anpi::Matrix<T> A_difference = A-A_aux; //Difference between original A and reconstructed-from-LU A
+	anpi::Matrix<T> A_difference = A-A_aux; //Difference between original A and reconstructed-from-LU A
 
-	//T norm = this->getNorm(A_difference);
-	T normA= this->getNorm(A);
-	T normA_aux = this->getNorm(A_aux);
+	T norm = this->getNorm(A_difference);
 
-
-	return normA-normA_aux;
+	return norm;
 
 }
 
 template<typename T>
 T TestDescomposition<T>::testQR(const anpi::Matrix<T> &A, anpi::Matrix<T> &Q, anpi::Matrix<T> &R){
 
+	ASSERT_EQ(A.rows(),A.cols()); //A matrix is square
+	ASSERT_EQ(Q.rows(),Q.cols()); //Q matrix is square
+	ASSERT_EQ(R.rows(),R.cols()); //R matrix is square
+
 	anpi::Matrix<T> A_aux(A.rows(),A.cols(),T(0));
 
 
 	A_aux = Q*R;
+	anpi::Matrix<T> A_difference = A-A_aux; //Difference between original A and reconstructed-from-QR A
 
-	//T norm = this->getNorm(A_difference);
-	T normA= this->getNorm(A);
-	T normA_aux = this->getNorm(A_aux);
+	T norm = this->getNorm(A_difference);
+	ASSERT_LT(norm,0.01);//Error less than 0.01%
 
-
-	return normA-normA_aux;
+	return norm;
 
 }
 
@@ -103,8 +105,9 @@ T TestDescomposition<T>::getNorm(anpi::Matrix<T> &M){
 	}
 
 	norm = sqrt(a_ij);
-	return norm;
+	ASSERT_LT(norm,0.01);//Error less than 0.01%
 
+	return norm;
 }
 
 #endif /* DESCOMPOSITION_TESTDESCOMPOSITION_H_ */
